2573f150dcd64f3556a82e847228bd99a527a025
Volodymyr Korzhevskyi <korzhevskyiv@gmail.com>
2025-05-22 02:27:49 +0200

    feat(models/user.js): make a first data model for user

---
+++ models/user.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: [true, "First name is required"],
+  },
+  lastName: {
+    type: String,
+    required: [true, "Last name is required"],
+  },
+  avatar: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: [true, "E-mail is required"],
+    validate: {
+      validator: function (email) {
+        return String(email)
+          .toLowerCase()
+          .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          );
+      },
+      message: (props = `Email ${props.value} is invalid!`),
+    },
+  },
+  password: {
+    type: String,
+  },
+  passwordChangedAt: {
+    type: Data,
+  },
+  passwordResetToken: {
+    type: String,
+  },
+  passwordResetExpires: {
+    type: Date,
+  },
+  createdAt: {
+    type: Data,
+  },
+  updatedAt: {
+    type: Data,
+  },
+});
+
+const User = new mongoose.model("User", userSchema);
+module.exports = User;
